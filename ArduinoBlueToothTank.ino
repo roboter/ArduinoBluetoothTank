c658772c8d51b17a6acc0dcde36dd167db80db5e
robby.roboter <roboter@begemotik.ee>
2018-07-15 15:52:02 +0300

    readme

---
+++ ArduinoBlueToothTank.ino
@@ -0,0 +1,111 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 1); // RX, TX
+// Define Motor Driver Pins
+
+int IN1 = 4;
+int IN2 = 5;
+
+int IN3 = 6;
+int IN4 = 9;
+int LED = 13;
+int state = 0;
+int motorSpeed = 100; // Normal Speed
+int motorTurnSpeed = 0; // Speed When Turning Left or Right
+int delayMs = 15000;
+void setup() {
+  mySerial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  pinMode(EN, OUTPUT); // Sets the pin as output
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(LED, OUTPUT);
+}
+void stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN, 0);
+
+  delay(100);
+}
+void loop() {  //if some data is sent, read it and save it in the state variable
+
+
+  //if (Serial.available() > 0)
+  if (mySerial.available() > 0)
+  {
+    //state = Serial.read();
+    state = mySerial.read();
+    Serial.println(state);
+    delay(10);
+    if (state == '0')
+    {
+      analogWrite(EN, 0);
+      delay(100);
+      state = 0;
+    }
+    else if (state == 0x31)     // If 1 Forwards
+    {
+      digitalWrite(LED, HIGH);
+      analogWrite(IN1, motorSpeed);
+      //digitalWrite(IN2, LOW);
+      //digitalWrite(IN3, HIGH);
+      analogWrite(IN3, motorSpeed);
+      digitalWrite(IN4, LOW);
+      digitalWrite(EN, LOW);
+      Serial.println("forward");
+      delay(100);
+      stop();
+      digitalWrite(LED, LOW);
+
+    }
+    else if (state == '2')      // If 2 Turn Left
+    {
+      digitalWrite(IN1, LOW);
+      analogWrite(IN2, motorSpeed);
+      analogWrite(IN3, motorSpeed);
+
+      digitalWrite(IN4, LOW);
+      //analogWrite(EN, motorTurnSpeed);
+
+      delay(100);
+      stop();
+    }
+    else if (state == '3') {    // If 3 Turn Right
+
+      analogWrite(IN1, motorSpeed);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      analogWrite(IN4, motorSpeed);
+      //analogWrite(EN, motorTurnSpeed);
+      delay(100);
+      stop();
+    }
+    else if (state == '4')       // If 4 Backwards
+    {
+      digitalWrite(IN1, LOW); // Motor drivers may have difference setting, change it accordingly
+      analogWrite(IN2, motorSpeed);
+      digitalWrite(IN3, LOW);
+      analogWrite(IN4, motorSpeed);
+      //analogWrite(EN, motorSpeed);
+
+      delay(100);
+      stop();
+    }
+
+    else if (state >= '9')
+    {
+
+      delay(100);
+      state = 0;
+    }
+  }
+}
--- readme.md
+++ readme.md
@@ -1,11 +1,27 @@
 # Description
+Control toy tank from any `android` phone with `bluetooth`. Build with `arduino`.
+Can be used to all types of tracks, that have 2 `motors`.
 # Hardware
-HC-05 Bluetooth module
-DRV8833
+###### HC-05 Bluetooth module
+![HC-05 Bluetooth module](images/HC-05.jpg)
+###### DRV8833
 [![drv8833](images/drv8833.jpg)](https://ebldc.com/?p=264)
-
+###### Track
+![track](images/track.jpg)
 
 
 # Branches
-L298N - original code with L298N
-[![Tank Bluetooth Controller](images/tank_bluetooth_controller.png)](https://play.google.com/store/apps/details?id=appinventor.ai_garylcyhk.Tank_Bluetooth_Controller_copy)
+###### main
+Inproved version that use `drv8833` and works with `bluetooth arduino track`
+1. Use `PWM` for speed control
+2. Stops motor after move. This approach mean that you have to press move keys constantly.
+
+###### L298N
+original code with L298N.
+This code sends code on button press and sends `s` code on button release.
+It working fine until you loose connection.
+# Android application
+###### [Bluetooth Arduino Tank Control](https://play.google.com/store/apps/details?id=appinventor.ai_garylcyhk.Tank_Bluetooth_Controller_copy)
+![Tank Bluetooth Controller](images/tank_bluetooth_controller.png)
+###### [Bluetooth Serial Monitor](https://play.google.com/store/apps/details?id=com.giumig.apps.bluetoothserialmonitor)
+![Bluetooth serial monitor](images/bluetoothserialmonitor.png)
