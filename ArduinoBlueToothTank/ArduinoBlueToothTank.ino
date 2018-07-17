9ac735c1bf9971eb1303c9469826066ff3604502
Aleksei Prokopov <robby.roboter@gmail.com>
2018-07-17 21:31:05 +0300

    AdvancedSerial added as a Lib

--- ArduinoBlueToothTank/ArduinoBlueToothTank.ino
+++ ArduinoBlueToothTank/ArduinoBlueToothTank.ino
@@ -1,5 +1,5 @@
+#include <../lib/advancedSerial/src/advancedSerial.h>
 #include <SoftwareSerial.h>
-
 SoftwareSerial mySerial(2, 1); // RX, TX
 // Define Motor Driver Pins
 
@@ -10,12 +10,13 @@ int IN3 = 6;
 int IN4 = 9;
 int LED = 13;
 int state = 0;
-int motorSpeed = 150; // Normal Speed
+int motorSpeed = 150;   // Normal Speed
 int motorTurnSpeed = 0; // Speed When Turning Left or Right
 int delayMs = 15000;
 void setup() {
-  mySerial.begin(9600);           // set up Serial library at 9600 bps
+  mySerial.begin(9600); // set up Serial library at 9600 bps
   Serial.begin(9600);
+  aSerial.setPrinter(Serial);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -34,17 +35,15 @@ void stop() {
 
   delay(100);
 }
-void loop() {  //if some data is sent, read it and save it in the state variable
+void loop() { // if some data is sent, read it and save it in the state variable
 
-
-  //if (Serial.available() > 0)
-  if (mySerial.available() > 0)
-  {
-    //state = Serial.read();
+  // if (Serial.available() > 0)
+  if (mySerial.available() > 0) {
+    // state = Serial.read();
     state = mySerial.read();
     Serial.println(state);
     delay(10);
-    if (state == 0x31)     // If 1 Forwards
+    if (state == 0x31) // If 1 Forwards
     {
       digitalWrite(LED, HIGH);
       analogWrite(IN1, motorSpeed);
@@ -56,8 +55,7 @@ void loop() {  //if some data is sent, read it and save it in the state variable
       stop();
       digitalWrite(LED, LOW);
 
-    }
-    else if (state == '2')      // If 2 Turn Left
+    } else if (state == '2') // If 2 Turn Left
     {
       digitalWrite(IN1, LOW);
       analogWrite(IN2, motorSpeed);
@@ -67,8 +65,7 @@ void loop() {  //if some data is sent, read it and save it in the state variable
 
       delay(100);
       stop();
-    }
-    else if (state == '3') {    // If 3 Turn Right
+    } else if (state == '3') { // If 3 Turn Right
 
       analogWrite(IN1, motorSpeed);
       digitalWrite(IN2, LOW);
@@ -76,10 +73,10 @@ void loop() {  //if some data is sent, read it and save it in the state variable
       analogWrite(IN4, motorSpeed);
       delay(100);
       stop();
-    }
-    else if (state == '4')       // If 4 Backwards
+    } else if (state == '4') // If 4 Backwards
     {
-      digitalWrite(IN1, LOW); // Motor drivers may have difference setting, change it accordingly
+      digitalWrite(IN1, LOW); // Motor drivers may have difference setting,
+                              // change it accordingly
       analogWrite(IN2, motorSpeed);
       digitalWrite(IN3, LOW);
       analogWrite(IN4, motorSpeed);
@@ -88,8 +85,7 @@ void loop() {  //if some data is sent, read it and save it in the state variable
       stop();
     }
 
-    else if (state >= '9')
-    {
+    else if (state >= '9') {
 
       delay(100);
       state = 0;
